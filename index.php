8b216f0a706613514dacc6a1072567fe5ef3b0a9
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-02-04 09:18:42 +0100

    ujhytgr

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
  * 1. Complétez $from et $to ( évitez d'utiliser la même adresse mail )
  * 2. Envoyez un mail avec ces informations, si certaines choses manquent, complétez
  * 3. Déployez sur votre serveur et testez !
- */
 
-$from = '';
-$to = '';
+*/
+
+$from = 'sam.coquelet@gmail.com';
+$to = 'coquelet.samuel@gmail.com';
 $message = 'Hello World, sending a simple mail !';
-// TODO Votre code ici.
-
+//// TODO Votre code ici.
+mail('coquelet.samuel@gmail.com','sujet','Hello World, sending a simple mail !');
 
 /**
  * 4. Commentez le code précédent, mais gardez les variables $from et $to
@@ -24,3 +25,12 @@ $message = 'Hello World, sending a simple mail !';
  *     N'écrasez pas les valeurs déjà existantes ( s'il y en a ).
  */
 // TODO Votre code ici.
+
+$file = 'mail.txt';
+$current = file_get_contents($file);
+
+
+//$message = 'Salut je suis sam et je t envoie ce mail pour te dire que tu va reussir  !
+// plus qu a envoyer ce mail !';
+//$debut = explode(" ",$message);
+
